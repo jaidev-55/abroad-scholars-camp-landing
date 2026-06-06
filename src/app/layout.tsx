103b74e4265a35a1068ffe229ca1c58b1a0da0d6
vc <vc@vcs-MacBook-Air.local>
2026-06-06 15:07:11 +0530

    updated-tag managers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,10 +75,34 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PWG8C59C');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body
         suppressHydrationWarning
         className="min-h-full flex flex-col font-sans"
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PWG8C59C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Google Ads / Analytics Tag */}
         <Script
           async
